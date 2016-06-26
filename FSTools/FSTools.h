917721710d3f0500318de7dd44e57b6ceef9a4d6
Thomas Moore <sundown@mac.com>
2016-06-26 17:21:04 -0500

    Initial Commit

---
+++ FSTools/FSTools.h
@@ -0,0 +1,19 @@
+//
+//  FSTools.h
+//  FSTools
+//
+//  Created by Thomas Moore on 6/26/16.
+//  Copyright © 2016 Thomas Moore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FSTools.
+FOUNDATION_EXPORT double FSToolsVersionNumber;
+
+//! Project version string for FSTools.
+FOUNDATION_EXPORT const unsigned char FSToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSTools/PublicHeader.h>
+
+
